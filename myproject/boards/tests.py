49c6eccb9a34d7f14d1bf5e8cf69075f722c57b3
girish <77683275+DarkShadowKonoha@users.noreply.github.com>
2022-10-23 12:21:45 +0530

    Adding the email field in the form

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,11 +1,14 @@
 from django.urls import re_path
 from django.contrib import admin
 
+from accounts import views as accounts_views
 from boards import views
+
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name ='new_topic'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^admin/', admin.site.urls),
 ]
