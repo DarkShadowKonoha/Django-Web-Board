b4d775e80e7208fc10d86fa26379aefbdc29fab9
girish <77683275+DarkShadowKonoha@users.noreply.github.com>
2022-10-23 12:46:54 +0530

    Logout

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,5 +1,6 @@
 from django.urls import re_path
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
 from boards import views
@@ -8,6 +9,7 @@ from boards import views
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^admin/', admin.site.urls),
